3093de0edbfad0146f25f277dbf42e8e30f579b6
Olayinka Oguntola <yinka.oguntola@gmail.com>
2021-02-15 19:20:42 +0100

    Final work

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,16 @@
+class HomeController < ApplicationController
+
+
+    def trash
+        @trash = List.where(:completed => true).destroy_all
+         redirect_to lists_url, notice: "All Marked Items Were Succesfully Deleted." 
+    end
+      
+    def trash_all
+        @trash_all = List.all.destroy_all
+        redirect_to lists_url, notice: "All Items Were Succesfully Deleted." 
+    end
+
+
+
+end
