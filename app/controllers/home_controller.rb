9bec07e74dbe093da9712084fdd0a7790ccf9a0e
Olayinka Oguntola <yinka.oguntola@gmail.com>
2021-02-16 19:21:46 +0100

    change path

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,12 +3,12 @@ class HomeController < ApplicationController
 
     def trash
         @trash = List.where(:completed => true).destroy_all
-         redirect_to lists_url, notice: "All Marked Items Were Succesfully Deleted." 
+         redirect_to root_url, notice: "All Marked Items Were Succesfully Deleted." 
     end
       
     def trash_all
         @trash_all = List.all.destroy_all
-        redirect_to lists_url, notice: "All Items Were Succesfully Deleted." 
+        redirect_to root_url, notice: "All Items Were Succesfully Deleted." 
     end
 
 
